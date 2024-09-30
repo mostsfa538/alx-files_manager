989a939f3cf4c15224daa00439296303592005c2
mostsfa538 <mostafa51moktar@gmail.com>
2024-09-30 20:16:14 +0300

    0.Redis utils

--- utils/redis.js
+++ utils/redis.js
@@ -5,29 +5,30 @@ class RedisClient {
     this.client = redis.createClient();
 
     this.client.on('error', (err) => {
-        console.error(err.message);
+      console.error(err.message);
     });
   }
 
   isAlive() {
-    return !this.client.connected;
+    return !!this.client.connected;
   }
 
   async get(key) {
     return new Promise((res, rej) => {
-      this.client.get(key, (err, reply)=> {
-          if (err) rej(err);
+      this.client.get(key, (err, reply) => {
+        if (err) {
+          rej(err.message);
+        } else {
           res(reply);
-        });
+        }
+      });
     });
   }
 
-  async set(key, value, duration) {
-    return new Promise((res, rej) => {
-      this.client.set(key, value, 'EX', duration, (err, reply) => {
-        if (err) rej(err);
-        res(reply);
-        });
+  async set(key, value, dur) {
+    return new Promise((res) => {
+      this.client.set(key, value, 'EX', dur);
+      res();
     });
   }
 
